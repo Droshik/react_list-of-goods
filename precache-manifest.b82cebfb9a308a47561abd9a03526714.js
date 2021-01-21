19a847c08cb1af608b16e4493c17c09566129528
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-21 08:04:56 +0200

    Updates

---
+++ precache-manifest.b82cebfb9a308a47561abd9a03526714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46729a2abd1d3fae7b17006c6fe1aed2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e4cfbd29fa39ecf52b9",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d7f2414e520e3fd3339f",
+    "url": "./static/js/2.720388b1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.720388b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4cfbd29fa39ecf52b9",
+    "url": "./static/js/main.449bd186.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
